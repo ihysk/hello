219a5df80c03a90ed93c72e982821094b7dba63e
sirouma <quest.for.curiosity1985@gmail.com>
2017-04-17 23:05:46 -0700

    Removed find_packages()

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='hello',
     version='0.0.1',
-    packages=find_packages(),
+    packages='',
     entry_points={
         'console_scripts': [
             'hello=hello.hello:hello',
