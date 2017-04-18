8a24e1ad36253aa847c9ab9d9edf15ded42b6f09
sirouma <quest.for.curiosity1985@gmail.com>
2017-04-17 22:43:25 -0700

    Simplify code in setup.py

--- setup.py
+++ setup.py
@@ -3,34 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='hello',
-
     version='0.0.1',
-
-    description='Hello!',
-    long_description='Just say Hello',
-
-    url='https://github.com/sirouma/hello',
-
-    author='siro_uma',
-    author_email='siro_uma@example.com',
-
-    license='MIT',
-
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Training',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    keywords='hello',
-
     packages=find_packages(),
-
     entry_points={
         'console_scripts': [
             'hello=hello.hello:hello',
